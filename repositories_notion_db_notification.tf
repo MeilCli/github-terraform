3aa6afab3ef3c34c255e790aaf6f835427b7a826
MeilCli <ics.sora.meil@gmail.com>
2022-02-13 23:26:33 +0900

    add notion-db-notification

---
+++ repositories_notion_db_notification.tf
@@ -0,0 +1,23 @@
+resource "github_repository" "notion-db-notification" {
+  name                   = "notion-db-notification"
+  description            = "This cli tool is that notify created or updated page of notion database"
+  has_projects           = false
+  has_wiki               = false
+  has_downloads          = true
+  has_issues             = true
+  is_template            = false
+  allow_rebase_merge     = false
+  delete_branch_on_merge = true
+  vulnerability_alerts   = true
+}
+
+locals {
+  collaborators_notion_db_notification = ["MeilCli-bot"]
+}
+
+resource "github_repository_collaborator" "notion_db_notification" {
+  depends_on = [github_repository.notion_db_notification]
+  for_each   = toset(local.collaborators_notion_db_notification)
+  repository = "notion-db-notification"
+  username   = each.value
+}
